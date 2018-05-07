a14607520612010a50278a6b769153ec4fc5a5cb
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2018-05-07 22:39:06 +0530

    Fixes

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-STAGED_CSS_FILES=`git diff --name-only --diff-filter=AMCR HEAD | grep css`
-echo $STAGED_CSS_FILES
+STAGED_CSS_FILES=`git diff --name-only --diff-filter=AMCR HEAD | grep \.css`
+#echo $STAGED_CSS_FILES
+
+for FILE in $STAGED_CSS_FILES
+do
+	if [ "$FILE" != "" ]
+	then
+	  FILEPATH="../"$FILE;
+	  #echo $FILEPATH;
+	  stylelint $FILEPATH --config=.stylelintrc --fix;
+	fi
+done
